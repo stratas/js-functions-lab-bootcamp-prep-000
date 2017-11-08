842f6192c69400c82e04a17a4ec611c79e966dc0
Jonathan Parris <jon8h1@yahoo.com>
2017-11-08 21:47:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return (`Happy holidays, ${name}!`)
 }
+
+happyHolidaysTo("Jonathan")
+
 function happyHolidaysTo(holiday, name){
   return (`Happy ${holiday}, ${name}!`)
 }
